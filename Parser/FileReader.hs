4d4adc2ba19ae778ae1947f814ef35823858e5d1
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2018-12-18 11:46:08 +0100

    Adding tons

---
+++ Parser/FileReader.hs
@@ -0,0 +1,34 @@
+module FileReader where
+
+-- MISSING :
+    -- Definition of ParseProg
+    -- Definition of Program Name
+{-
+import System.IO
+import Parser
+--import ParseProg
+
+readF :: IO String
+readF = do inh <- openFile "input.txt" ReadMode
+           prog <- readloop inh
+           hClose inh
+           return prog
+
+main :: IO (Program Name)
+main = do inp <- readF
+          return (comp (parse parseProg inp)) -- call to parseProg
+
+comp :: [(Program Name, Name)] -> Program Name
+comp []         = error "no parse"
+comp [(e,[])]   = e
+comp [(_,a)]    = error ("Doesn't use all input" ++ a)
+
+readloop inh = do ineof <- hIsEOF inh
+                  if ineof
+                    then return []
+                  else do
+                        x  <- hGetLine inh
+                        xs <- readloop inh
+                        return (x++xs)
+
+- }
--- Parser/Parser.hs
+++ Parser/Parser.hs
@@ -1,53 +1,8 @@
 module Parser where
 
 import Expr
-import Control.Applicative
-import Data.Char
+import Parser_Utils
 
-newtype Parser a = P (String -> [(a, String)])
-
-parse :: Parser a -> String -> [(a,String)]
-parse (P p) inp = p inp
-
-item :: Parser Char
-item = P (\inp -> case inp of
-                []     -> []
-                (x:xs) -> [(x,xs)])
-
-instance Functor Parser where
-    -- fmap :: (a -> b) -> Parser a -> Parser b
-    fmap g p = P (\inp -> case parse p inp of
-                            []        -> []
-                            [(v,out)] -> [(g v, out)])
-
-instance Applicative Parser where
-    -- pure :: a -> Parser a
-    pure v = P (\inp -> [(v,inp)])
-
-    -- <*> :: Parser (a -> b) -> Parser a -> Parser b
-    pg <*> px = P (\inp -> case parse pg inp of
-                            []        -> []
-                            [(g,out)] -> parse (fmap g px) out)
-
-instance Monad Parser where
-    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-    p >>= f = P (\inp -> case parse p inp of
-                        [] -> []
-                        [(v,out)] -> parse (f v ) out)
-
-instance Alternative Parser where
-    -- empty :: Parser a
-    empty = P(\inp -> [])
-
-    -- (<|>) :: Parser a -> Parser a -> Parser a
-    p <|> q = P (\inp -> case parse p inp of
-                        []        -> parse q inp
-                        [(v,out)] -> [(v,out)])
-
-
-sat :: (Char -> Bool) -> Parser Char
-sat p = do x <- item
-           if p x then return x else empty
 
 
 -- ESEMPI
