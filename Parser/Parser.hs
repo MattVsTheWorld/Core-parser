d3fc1b24bd0d908b1e0a03e220ff6ef41f4a52d9
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2018-12-23 17:42:50 +0100

    bozza, types are all wrong

--- Parser/Parser.hs
+++ Parser/Parser.hs
@@ -5,6 +5,7 @@ import Parser_Utils
 
 import Control.Applicative
 import Data.Char
+import Data.List
 
 {-
 IMPORTANT NOTES:
@@ -17,26 +18,6 @@ IMPORTANT NOTES:
   -- maybe there should be some choices? (see parseProg)
   -- are we using empty at all? Am I retarded?
 -}
--- now calls ident which considers '_'
-{-
--- Need Parser (EVar Name), not Parser String
-exprIdent :: Parser (Expr Name) 
-exprIdent = do x  <- lower
-               xs <- many varch
-               let a = EVar (x:xs) -- make into evar
-               return a
-
--- SUSPICION : should be string. wat do? FIXED
-parseVar :: Parser (Expr Name)
-parseVar = token exprIdent
--}
--- 
-{- 
-parseNum :: Parser (Expr Name)
-parseNum = do xs <- some digit -- 1 or more digits
-              return (ENum (read xs)) -- read per trasformarlo in Int
-
--}
 
 -- ESEMPI
 parseProg :: Parser (Program Name)
@@ -45,7 +26,6 @@ parseProg = do p <- parseScDef
                   ps <- parseProg
                   return (p:ps)
                  <|> return [p]
-
 {-
 Supercombinator = definizione di funzione
 banana   x   xs     =       x + xs
@@ -59,8 +39,6 @@ parseScDef = do v  <- identifier      -- = parseVar
                 character '='       -- throw away
                 body <- parseExpr   -- DEFINE
                 return (v, pf, body)
-
-
 -- Def -> var = expr
 -- Needs a checkup
 parseDef :: Parser (Def Name)
@@ -68,7 +46,6 @@ parseDef = do v <- identifier
               character '='
               body <- parseExpr
               return (v, body) -- Def Name is a tuple
-
 
 -- EConstr Int Int
 parseConstr :: Parser (Expr Name)
@@ -80,12 +57,23 @@ parseConstr = do symbol "Pack"
                  character '}'
                  return (EConstr tag arity)
 
+-- <num> var1_n -> expr
+-- Alter a = (Int, [a], Expr a)
+parseAlt :: Parser (Alter Name)
+parseAlt = do character '<'
+              n <- integer
+              character '>'
+              vs <- many identifier
+              symbol "->"
+              e <- parseExpr
+              return (n,vs,e)
+
 -- AExpr -> var | num | Pack{num,num} | (expr)
 parseAExpr :: Parser (Expr Name)
 parseAExpr = do v <- identifier
                 return (EVar v)
                <|>
-             do n <- integer -- # only works for integers -- fract?
+             do n <- integer -- could expand to fractals
                 return (ENum n)
                <|>
              do c <- parseConstr
@@ -140,19 +128,109 @@ parseExpr = {- Nope
                return (ELam vs e)
               <|>
               -- atomic
-            parseAExpr -- expr1? ##
+              parseAExpr
+              -- ==== expr1 ***
+            --parseExpr1 --
 
 {-
-parseExpr1 :: Parser (Expr Name)
-parseExpr1 = do parseExpr2
-                symbol <- '|'
-                parseExpr1
-               <|> parseExpr2
+-- ***
+-- expr1 -> expr2 || expr1 | expr2
+-- OR 
+-- associativity = right
+parseExpr1 :: Parser (Expr Name) 
+parseExpr1 = do e2 <- parseExpr2 -- ma non e' il tipo giusto!!
+                character '|'
+                e1 <- parseExpr1
+                return (e1 ++ " | " ++ e2)
+               <|> 
+                parseExpr2
+
+-- expr2 -> expr3 & expr2 | expr3
+-- AND
+-- associativity = right
 parseExpr2 :: Parser (Expr Name)
+parseExpr2 = do e3 <- parseExpr3
+                character '&'
+                e2 <- parseExpr2
+                return (e3 ++ " & " ++ e2)
+               <|> 
+                parseExpr3
+
+-- expr3 -> expr4 relop expr4 | expr 4
+-- associativity = none
+-- RELOP
+-- relop = [< | <= | == | ~= | >= | >]
+relop = ["<","<=","==","~=",">=",">"]
 parseExpr3 :: Parser (Expr Name)
+parseExpr3 = do el <- parseExpr4
+                rel <- symbol any relop -- NICE TRY LUL
+                er <- parseExpr4
+                return (el ++ " " ++ rel ++ " " ++ er)
+               <|> 
+                parseExpr4 
+
+
+-- expr4 -> expr5 + exp4 | expr5 - expr5 | expr5
+-- ADDITION, DIFFERENCE
+-- associativity = right, none
 parseExpr4 :: Parser (Expr Name)
+parseExpr4 = do e5 <- parseExpr5
+                character '+'
+                e4 <- parseExpr4
+                return (e5 ++ " + " ++ e4)
+               <|>
+                el <- parseExpr5
+                character '-'
+                er <- parseExpr5
+                return (el ++ " - " ++ er)
+               <|>
+                parseExpr5
+
+
+-- expr5 -> expr6 * expr5 | expr6 / expr6 | expr6
+-- MULTIPLICATION, DIVISION
+-- associativity = right, none
 parseExpr5 :: Parser (Expr Name)
+parseExpr5 = do e6 <- parseExpr6
+                character '*'
+                e5 <- parseExpr5
+                return (e6 ++ " * " ++ e5)
+               <|>
+                el <- parseExpr6
+                character '/'
+                er <- parseExpr6
+                return (el ++ " / " ++ er)
+               <|>
+                parseExpr6
+-- expr6 -> aexpr_1... aepxr_n (n>=1)
+-- associativity = left
+-- APPLICATION
+-- in pratica ritorna una lista di Expr Names
+-- problema peculiare... 
+-- se e' una, e' atomica (application to nothing? mmh), else e' un'applicazione
 parseExpr6 :: Parser (Expr Name)
+parseExpr6 = do es <- some parseAExpr
+                return es
+-}
+
+{-
+-- AExpr -> var | num | Pack{num,num} | (expr)
+parseAExpr :: Parser (Expr Name)
+parseAExpr = do v <- identifier
+                return (EVar v)
+               <|>
+             do n <- integer -- # only works for integers -- fract?
+                return (ENum n)
+               <|>
+             do c <- parseConstr
+                return (c)
+               <|>
+             do character '('
+                e <- parseExpr -- correct? mmmh
+                character ')'
+                return (e)
+             -- ( expr ) 
+          
 -}
 
 
@@ -167,18 +245,6 @@ parseBin = do a <- character '+'
 -}
               -- relop
               -- boolop
-
--- <num> var1_n -> expr
--- Alter a = (Int, [a], Expr a)
-parseAlt :: Parser (Alter Name)
-parseAlt = do character '<'
-              n <- integer
-              character '>'
-              vs <- many identifier
-              symbol "->"
-              e <- parseExpr
-              return (n,vs,e)
--- lambda
 
 -- TEST
 -- test_prog = "f = 3; g x y = let z = x in z; h x = case (let y = x in y) of  <1> -> 2 <2> -> 5"
